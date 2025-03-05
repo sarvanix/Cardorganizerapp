c644aa12a013d08c247af0db03d9819ee293f90e
sarvanix <ltirumalasetti1@student.gsu.edu>
2025-03-05 13:37:33 -0500

    Add and delete folder functionality in UI

--- lib/screens/folder_screen.dart
+++ lib/screens/folder_screen.dart
@@ -6,7 +6,7 @@ class FolderScreen extends StatefulWidget {
   @override
   _FolderScreenState createState() => _FolderScreenState();
 }
-
+//sharuu
 class _FolderScreenState extends State<FolderScreen> {
   List<Map<String, dynamic>> _folders = [];
 
@@ -21,6 +21,40 @@ class _FolderScreenState extends State<FolderScreen> {
     setState(() {
       _folders = data;
     });
+  }
+
+  Future<void> _addFolder() async {
+    TextEditingController folderController = TextEditingController();
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Add New Folder"),
+          content: TextField(
+            controller: folderController,
+            decoration: InputDecoration(hintText: "Enter folder name"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                if (folderController.text.isNotEmpty) {
+                  await DatabaseHelper.instance.addFolder(folderController.text);
+                  _loadFolders();
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text("Add"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _deleteFolder(int id) async {
+    await DatabaseHelper.instance.deleteFolder(id);
+    _loadFolders();
   }
 
   @override
@@ -41,21 +75,36 @@ class _FolderScreenState extends State<FolderScreen> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => CardScreen(folderId: _folders[index]['id'], folderName: _folders[index]['name']),
+                  builder: (context) => CardScreen(
+                    folderId: _folders[index]['id'],
+                    folderName: _folders[index]['name'],
+                  ),
                 ),
               );
             },
             child: Card(
               color: Colors.blueAccent,
-              child: Center(
-                child: Text(
-                  _folders[index]['name'],
-                  style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _folders[index]['name'],
+                    style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  IconButton(
+                    icon: Icon(Icons.delete, color: Colors.white),
+                    onPressed: () => _deleteFolder(_folders[index]['id']),
+                  ),
+                ],
               ),
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addFolder,
+        child: Icon(Icons.add),
       ),
     );
   }
