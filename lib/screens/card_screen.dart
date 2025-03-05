f6132368bd063f1a4bc2c395af670fee0a0a298d
sarvanix <ltirumalasetti1@student.gsu.edu>
2025-03-05 13:41:26 -0500

    Enforce folder limits: Min 3, Max 6 cards per folder

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -28,6 +28,13 @@ class _CardScreenState extends State<CardScreen> {
   }
 
   Future<void> _addCard() async {
+    if (_cards.length >= 6) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("This folder can only hold 6 cards.")),
+      );
+      return;
+    }
+
     TextEditingController nameController = TextEditingController();
     TextEditingController suitController = TextEditingController();
     TextEditingController imageUrlController = TextEditingController();
@@ -67,46 +74,14 @@ class _CardScreenState extends State<CardScreen> {
     );
   }
 
-  Future<void> _updateCard(int id, String currentName, String currentSuit, String currentImageUrl) async {
-    TextEditingController nameController = TextEditingController(text: currentName);
-    TextEditingController suitController = TextEditingController(text: currentSuit);
-    TextEditingController imageUrlController = TextEditingController(text: currentImageUrl);
-
-    await showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Update Card"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(controller: nameController, decoration: InputDecoration(hintText: "Enter card name")),
-              TextField(controller: suitController, decoration: InputDecoration(hintText: "Enter card suit")),
-              TextField(controller: imageUrlController, decoration: InputDecoration(hintText: "Enter image URL")),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                await DatabaseHelper.instance.updateCard(
-                  id,
-                  nameController.text,
-                  suitController.text,
-                  imageUrlController.text,
-                  widget.folderId,
-                );
-                _loadCards();
-                Navigator.of(context).pop();
-              },
-              child: Text("Update"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   Future<void> _deleteCard(int id) async {
+    if (_cards.length <= 3) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("You need at least 3 cards in this folder.")),
+      );
+      return;
+    }
+
     await DatabaseHelper.instance.deleteCard(id);
     _loadCards();
   }
@@ -126,23 +101,9 @@ class _CardScreenState extends State<CardScreen> {
                   : Icon(Icons.image_not_supported),
               title: Text(_cards[index]['name']),
               subtitle: Text(_cards[index]['suit']),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.edit, color: Colors.blue),
-                    onPressed: () => _updateCard(
-                      _cards[index]['id'],
-                      _cards[index]['name'],
-                      _cards[index]['suit'],
-                      _cards[index]['image_url'] ?? '',
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () => _deleteCard(_cards[index]['id']),
-                  ),
-                ],
+              trailing: IconButton(
+                icon: Icon(Icons.delete, color: Colors.red),
+                onPressed: () => _deleteCard(_cards[index]['id']),
               ),
             ),
           );
